cbf5932d7f4fd90a849754e148a0ecf3bb0910c5
Thang Tong <tongthang0104@gmail.com>
2015-10-22 16:34:55 -0600

    Hierachy Step And Configure the Task.swift

---
+++ Task/TaskController.swift
@@ -0,0 +1,24 @@
+//
+//  TaskController.swift
+//  Task
+//
+//  Created by Thang H Tong on 10/22/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class TaskController {
+    
+    static var tasksArray: [Task] = []
+    
+    func completedTasks(task: [Task]) -> Task {
+        for taskCompleted in 
+            
+        }
+    }
+    
+    
+    
+    
+}
