f7d211a98d3e5fb98e8f6e719d58ebe093b7b6f0
Thang Tong <tongthang0104@gmail.com>
2015-10-23 17:54:10 -0600

    DateHelper

---
+++ Task/DateHelper.swift
@@ -0,0 +1,20 @@
+//
+//  DateHelper.swift
+//  Task
+//
+//  Created by Thang H Tong on 10/23/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+extension NSDate {
+    
+    func stringValue() -> String {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .MediumStyle
+        
+        return formatter.stringFromDate(self)
+    }
+    
+}
