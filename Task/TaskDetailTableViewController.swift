3cec041eb2c93f051e01d70fa529bc3bb45975bb
Thang Tong <tongthang0104@gmail.com>
2015-10-25 18:36:38 -0600

    -

--- Task/TaskDetailTableViewController.swift
+++ Task/TaskDetailTableViewController.swift
@@ -107,14 +107,7 @@ class TaskDetailTableViewController: UITableViewController, UITextFieldDelegate 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    
-    
-    
-    
-    
-    
+    } 
     
         /*
     // MARK: - Navigation
