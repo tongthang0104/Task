f0f842667ff135ce74ce71c7efc604445c0424cb
Andrew Porter <haporter@me.com>
2016-02-11 19:00:18 -0700

    Implementing Task model

---
+++ Task/TaskDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TaskDetailViewController.swift
+//  Task
+//
+//  Created by Andrew Porter on 2/11/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+}
---
+++ Task/TaskListTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TaskListTableViewController.swift
+//  Task
+//
+//  Created by Andrew Porter on 2/11/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class TaskListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
